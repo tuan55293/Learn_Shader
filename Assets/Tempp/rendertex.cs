05defb3fa373f815965ebfcad8f85755c9a9fdfa
Lê Văn Tuấn Anh <tuan55293@gmail.com>
2023-10-17 20:06:00 +0700

    update

---
+++ Assets/Tempp/rendertex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rendertex : MonoBehaviour
+{
+    public RenderTexture rt;
+    public RawImage raw;
+
+    public Texture2D tex;
+    static public Texture2D GetRTPixels(RenderTexture rt)
+    {
+        // Remember currently active render texture
+        RenderTexture currentActiveRT = RenderTexture.active;
+
+        // Set the supplied RenderTexture as the active one
+        RenderTexture.active = rt;
+
+        // Create a new Texture2D and read the RenderTexture image into it
+        Texture2D tex = new Texture2D(rt.width, rt.height);
+        tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), 0, 0);
+        tex.Apply();
+
+        // Restore previously active render texture
+        RenderTexture.active = currentActiveRT;
+        return tex;
+    }
+    private void Start()
+    {
+        tex = GetRTPixels(rt);
+        raw.texture = tex;
+    }
+}
